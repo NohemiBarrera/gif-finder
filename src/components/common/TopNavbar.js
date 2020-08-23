b8d522ea6a693a61229790a87d79faeba0567da3
Nohemi Barrera <nohemi.baol@gmail.com>
2020-08-22 23:14:56 -0500

    agrega link a top gifs

--- src/components/common/TopNavbar.js
+++ src/components/common/TopNavbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from 'react-bootstrap/Nav';
+import {Link} from 'react-router-dom';
 
 const TopNavbar = (props) => {
   return (
     <Navbar bg="dark" variant="dark">
       <Nav>
-        <Nav.Link to="/">Top Gifs</Nav.Link>
+        <Link to="/" className="text-white">Top Gifs</Link>
       </Nav>
     </Navbar>
   );
